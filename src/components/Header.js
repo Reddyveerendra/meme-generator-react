a90d61ca24fb9e2eca57fb5d6a9394476e13a78a
Reddyveerendra <reddyveerendra535@gmail.com>
2023-04-12 15:30:11 +0530

    final commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export let Header = () => {
     return (
         <div className="header">
             <img src="https://raw.githubusercontent.com/Reddyveerendra/meme-generator-react/main/src/images/logo.gif" alt="logo" />
-            <p>Meme Generator</p>
+            <p>Meme Maker</p>
         </div>
     )
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -7,14 +7,27 @@ function Meme() {
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         setImageUrl(memesArray[randomNumber].url);
     }
+    const [meme, setMeme] = React.useState({ topText: "", bottomText: "" })
+    function handleChange(e) {
+        const { name, value } = e.target
+        setMeme(prev => {
+            return {
+                ...prev, [name]: value
+            }
+        })
+    }
     return (
         <div>
             <div className="form">
-                <input type="text" placeholder="Top text" className="form_inputs" />
-                <input type="text" placeholder="Bottom text" className="form_inputs" />
+                <input type="text" placeholder="Top text" className="form_inputs" name="topText" value={meme.topText} onChange={handleChange} />
+                <input type="text" placeholder="Bottom text" className="form_inputs" name="bottomText" value={meme.bottomText} onChange={handleChange} />
                 <button className="form_button" onClick={getMemeImage}>Get a new Meme image 🖼</button>
             </div>
-            <img src={imageUrl} alt="" className="meme_img" />
+            <div className="meme">
+                <img src={imageUrl} alt="" className="meme_img" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
